0cc02270f41ad542153be9161244fc0e147e941b
heejung-newheee <aldls2311@gmail.com>
2025-12-17 16:53:01 +0900

    feat: supabase setting

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -71,7 +71,7 @@ export const SignUp = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="example@email.com"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg  focus:ring-blue-500"
               required
             />
           </div>
